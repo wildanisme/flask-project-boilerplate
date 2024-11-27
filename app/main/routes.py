149eca879ddd781eb4ea9fa28f8315fa9a0561e4
wildanisme <wildanisme.com@@gmail.com>
2024-11-27 20:29:50 +0700

    adj: add about route

--- app/main/routes.py
+++ app/main/routes.py
@@ -4,3 +4,7 @@ from flask import render_template, url_for
 @bp.route('/')
 def index():
     return render_template("index.html")
+
+@bp.route('/about')
+def about():
+    return render_template("about.html")
